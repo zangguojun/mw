4c5edf921339025e21dd1090b7d4220925aae983
tong <lbuchiyu@gmail.com>
2022-12-14 00:13:55 +0800

    feat: 删除ffc相关逻辑

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -1,14 +1,10 @@
 import { Context } from '@midwayjs/koa';
 import {
   Controller,
-  Files,
   Get,
   Inject,
-  Post,
-  Query,
 } from '@midwayjs/decorator';
 import { QueueService } from '@midwayjs/task';
-import { ProgressTask } from '../task/progress.task';
 
 @Controller('/')
 export class HomeController {
@@ -28,25 +24,5 @@ export class HomeController {
   @Get('/hello')
   async hello(): Promise<string> {
     return 'hello world';
-  }
-
-  @Get('/get-queue')
-  async getQueue(@Query('id') id: string) {
-    return this.queueService.getClassQueue(ProgressTask).getJob(id);
-  }
-
-  /**
-   * @param files
-   *  filename: 文件原名
-   *  data: mode 为 file 时为服务器临时文件地址
-   *  fieldname: 表单 field 名
-   *  mimeType: mime
-   */
-  @Post('/upload')
-  async upload(@Files() files) {
-    const [file] = files;
-    const n = file?.data?.split('/');
-    file.url = n.slice(n.length - 3).join('/');
-    return file;
   }
 }
--- src/interface.ts
+++ src/interface.ts
@@ -2,25 +2,6 @@ export interface IUserOptions {
   name: string;
 }
 
-export interface IFFCOptions {
-  id: string;
-}
-
-export interface IFFCProgress {
-  taskId: string;
-  progress: number;
-  state: string;
-}
-
-export interface IFFCParams {
-  taskId: string;
-}
-
-export interface IFFCBody {
-  id: string;
-  images: string[];
-}
-
 export interface IRst<T> {
   code: number;
   msg: string;
